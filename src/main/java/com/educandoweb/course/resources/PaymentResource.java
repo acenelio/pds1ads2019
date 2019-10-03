21cb8098005c9e74c6840686effeb08475f1b6da
Nelio Alves <acenelio@gmail.com>
2019-10-03 16:19:10 -0300

    Payment PUT POST endpoints

--- src/main/java/com/educandoweb/course/resources/PaymentResource.java
+++ src/main/java/com/educandoweb/course/resources/PaymentResource.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.dto.PaymentDTO;
 import com.educandoweb.course.services.PaymentService;
@@ -33,4 +38,20 @@ public class PaymentResource {
 		PaymentDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PostMapping
+	public ResponseEntity<PaymentDTO> insert(@RequestBody PaymentDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<PaymentDTO> update(@PathVariable Long id, @RequestBody PaymentDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}	
 }
--- src/main/java/com/educandoweb/course/services/PaymentService.java
+++ src/main/java/com/educandoweb/course/services/PaymentService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.educandoweb.course.dto.PaymentDTO;
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.Payment;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.PaymentRepository;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
@@ -17,6 +22,9 @@ public class PaymentService {
 
 	@Autowired
 	private PaymentRepository repository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 	
 	public List<PaymentDTO> findAll() {
 		List<Payment> list = repository.findAll();
@@ -27,5 +35,30 @@ public class PaymentService {
 		Optional<Payment> obj = repository.findById(id);
 		Payment entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		return new PaymentDTO(entity);
+	}
+	
+	@Transactional
+	public PaymentDTO insert(PaymentDTO dto) {
+		Order order = orderRepository.getOne(dto.getOrderId());
+		Payment payment = new Payment(null, dto.getMoment(), order);
+		order.setPayment(payment);
+		orderRepository.save(order);
+		return new PaymentDTO(order.getPayment());		
+	}
+	
+	@Transactional
+	public PaymentDTO update(Long id, PaymentDTO dto) {
+		try {
+			Payment entity = repository.getOne(id);
+			updateData(entity, dto);
+			entity = repository.save(entity);
+			return new PaymentDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
+	}
+
+	private void updateData(Payment entity, PaymentDTO dto) {
+		entity.setMoment(dto.getMoment());
 	}
 }
